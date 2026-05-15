6679524596773fa062f4474eeb6135d3ca96b248
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2026-05-15 20:55:36 +0600

    Lesson 6

--- db/main_db.py
+++ db/main_db.py
@@ -2,7 +2,9 @@ import sqlite3
 from config import path_db
 from db import queries
 
-
+a = ()
+b = []
+c = {}
 
 def init_db():
     conn = sqlite3.connect(path_db)
@@ -11,3 +13,33 @@ def init_db():
     print('БД подключена!')
     conn.commit()
     conn.close()
+
+
+# def add_task(task):
+#     conn = sqlite3.connect(path_db)
+#     cursor = conn.cursor()
+#     # cursor.execute('INSERT INTO tasks (task) VALUES (?)', (task, ))
+#     cursor.execute(queries.insert_task, (task,))
+#     conn.commit()
+#     task_id = cursor.lastrowid
+#     conn.close()
+#     return task_id
+
+def add_task(task):
+    with sqlite3.connect(path_db) as conn: 
+        cursor = conn.cursor()
+        cursor.execute(queries.insert_task, (task, ))
+        task_id = cursor.lastrowid
+    return task_id
+
+
+def update_task(task_id, new_task=None):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+
+    if new_task is not None:
+        cursor.execute(queries.update_task, (new_task, task_id))
+    
+    conn.commit()
+    conn.close()
+
